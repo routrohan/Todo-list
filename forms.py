d8bf1b9de400e77e93f6a8f4f0e87bcefee0afcc
Rohan Rout <rohan.rout98@gmail.com>
2020-06-16 15:33:41 +0530

    adding edit and delete functionalities

--- forms.py
+++ forms.py
@@ -6,3 +6,6 @@ from wtforms.validators import DataRequired
 class AddTaskForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     submit= SubmitField('Submit')
+
+class DeleteTaskForm(FlaskForm):
+    submit = SubmitField('Delete')
